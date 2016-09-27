11648358cd33d30960c786320066854f72365b5d
Matheus Schmitz <augs.math@gmail.com>
2016-09-27 17:00:41 -0300

    Codigos com exercicios

---
+++ modulo-01/Elfo.java
@@ -0,0 +1,41 @@
+public class Elfo{
+    //Atributos
+    private String nome;
+    private Item arco;
+    private Item flecha;
+    private int exp;
+    
+    //Metodos
+    public Elfo(String n, int flechas){
+        if(flechas == 0){
+            flechas += 42;
+        }
+        nome = n;
+        arco = new Item("Arco", 1);
+        flecha = new Item("Flecha", flechas);
+        exp = 0;
+    }
+    
+    public void atirarFlechaRefactory(boolean acertar){
+        flecha.setQuantidade(flecha.getQuantidade()-1);
+        if(acertar){
+            exp++;
+        }
+    }
+    
+    public void plusExp(double valor){
+        exp += valor;
+    }    
+    
+    //Getters and Setters
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String n){
+        nome = n;
+    }
+    
+    
+    
+}
