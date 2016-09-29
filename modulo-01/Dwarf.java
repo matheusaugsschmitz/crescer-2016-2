c9c41138dd5e050d1997b59f20843bc309dfc4d3
Matheus Schmitz <augs.math@gmail.com>
2016-09-28 22:02:51 -0300

    feat(Dwarf): Dwarf tem nome e data de nascimento

--- modulo-01/Dwarf.java
+++ modulo-01/Dwarf.java
@@ -1,11 +1,20 @@
 public class Dwarf{
     //ATRIBUTOS
     private int vida = 110;
+    private DataTerceiraEra dataNascimento;
+    private String nome;
+    
+    //CONSTRUTOR
+    public Dwarf(String nome, DataTerceiraEra dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
     
     //METODOS
     public void perderVida(){
         vida -= 10;
     }
+    
     
     //GETTERS AND SETTERS
     public int getVida(){
