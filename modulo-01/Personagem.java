752a5b52c249d082b46d45745eaebec94eaa1cba
Matheus Schmitz <augs.math@gmail.com>
2016-10-04 17:30:56 -0300

    feat(Personagem): alterar a classe para abstract

--- modulo-01/Personagem.java
+++ modulo-01/Personagem.java
@@ -5,7 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Personagem{
+public abstract class Personagem{
     protected String nome;
     protected int exp;
     protected Status status;
