a3d276a0b12c1e177b863497bb144e740997ea0d
Matheus Schmitz <augs.math@gmail.com>
2016-10-25 14:55:16 -0200

    feat(Exercicio 2): corrigido

--- modulo-04/JS-Dia-01/exercicio2.js
+++ modulo-04/JS-Dia-01/exercicio2.js
@@ -1,18 +1,20 @@
 //EXERCICIO 2
 var diglettDig = function(){
-  for (var i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 100; i++) {
+    let texto;
     if(i % 3 === 0 || i % 5 === 0){
       if(i % 3 === 0 && i % 5 !== 0){
-        console.log('Diglett dig');
+        texto = 'Diglett dig';
       }else{
         if(i%3 !== 0 && i%5 === 0){
-          console.log('trio trio trio');
+          texto = 'trio trio trio';
         }else{
-          console.log('Diglett dig, trio trio trio');
+          texto = 'Diglett dig, trio trio trio';
         }
       }
     }else{
-      console.log(i);
+      texto = i;
     }
+    console.log(texto)
   }
 }
