4f83ddc6d5772733968883424a335edfa4b309eb
Matheus Schmitz <augs.math@gmail.com>
2016-10-25 14:00:17 -0200

    feat(Exercicio 1): corrigido

--- modulo-04/JS-Dia-01/exercicio1.js
+++ modulo-04/JS-Dia-01/exercicio1.js
@@ -1,6 +1,6 @@
 //EXERCICIO 1
 var gerarPiramide = function(niveis){
-  for(var i = 0; i < niveis; i++){
-    console.log(Array(i+2).join('R$'));
+  for(var i = 1; i <= niveis; i++){
+    console.log(Array(i+1).join('R$'));
   }
 }
