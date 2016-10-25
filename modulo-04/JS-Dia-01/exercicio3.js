754770e68bcda70317ea7ef8717fc5f1b34ccedd
Matheus Schmitz <augs.math@gmail.com>
2016-10-25 14:55:45 -0200

    feat(Exercicio 3): corrigido

--- modulo-04/JS-Dia-01/exercicio3.js
+++ modulo-04/JS-Dia-01/exercicio3.js
@@ -1,9 +1,9 @@
 //EXERCICIO 3
-var find = function(array, opt) {
-  if(typeof opt === "function"){
-    var retorno = [];
-    for (var i = 0, len = array.length; i < len; i++) {
-        if(opt(array[i])){
+var find = function(array, fnFiltro) {
+  if(typeof fnFiltro === "function"){
+    let retorno = [];
+    for (let i = 0, len = array.length; i < len; i++) {
+        if(fnFiltro(array[i])){
           retorno.push(array[i]);
         }
     }
