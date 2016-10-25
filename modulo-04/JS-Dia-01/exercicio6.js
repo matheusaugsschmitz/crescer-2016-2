496bfac835d3eb285e81135021550cb695d9b07d
Matheus Schmitz <augs.math@gmail.com>
2016-10-25 17:11:52 -0200

    feat(Exercicio 6): INCOMPLETO

--- modulo-04/JS-Dia-01/exercicio6.js
+++ modulo-04/JS-Dia-01/exercicio6.js
@@ -3,11 +3,11 @@ var ctrlC = function(param){
   var retorno;
   if(typeof param === 'array' || typeof param === 'object'){
     retorno = typeof param === 'array' ? [] : {};
-    for(int i = 0, len = param.length; i < len; i++){
-      if(typeof param[i] === 'object'){
-        retorno[i] = ctrlC(param[i]);
+    for(let prop in param){
+      if(typeof param[prop] === 'object'){
+        retorno.push(ctrlC(param[prop]));
       }else{
-        retorno[i] = para[i];
+        retorno.push(param);
       }
     }
   }else{
