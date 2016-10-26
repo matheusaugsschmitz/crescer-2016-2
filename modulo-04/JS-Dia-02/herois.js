71384669d12c96c4d9762b6ebbf0daa0841f5ddb
Matheus Schmitz <augs.math@gmail.com>
2016-10-25 23:48:40 -0200

    feat(marvel, herois .js):CÓDIGO INCOMPLETO

--- modulo-04/JS-Dia-02/herois.js
+++ modulo-04/JS-Dia-02/herois.js
@@ -1,7 +1,17 @@
 class Heroi{
   constructor(){
-    for (var i = 0; i < arguments.length; i++) {
-      arguments[i]
+    for (let i = 0; i < arguments.length; i++) {
+      this.comics = arguments[i]['comics'];
+      this.description = arguments[i]['description'];
+      this.events = arguments[i]['events'];
+      this.id = arguments[i]['id'];
+      this.modified = arguments[i]['modified'];
+      this.name = arguments[i]['name'];
+      this.resourceURI = arguments[i]['resourceURI'];
+      this.series = arguments[i]['series'];
+      this.stories = arguments[i]['stories'];
+      this.thumbnail = arguments[i]['thumbnail'];
+      this.urls = arguments[i].urls;
     }
   }
 }
