fb5326ac3dea254872f9c9122d4b8b1a1febc9ce
Matheus Schmitz <augs.math@gmail.com>
2016-10-26 13:35:58 -0200

    feat(Herois): criar classe de heróis com método construtor

--- modulo-04/JS-Dia-02/herois.js
+++ modulo-04/JS-Dia-02/herois.js
@@ -1,17 +1,8 @@
-class Heroi{
-  constructor(){
-    for (let i = 0; i < arguments.length; i++) {
-      this.comics = arguments[i]['comics'];
-      this.description = arguments[i]['description'];
-      this.events = arguments[i]['events'];
-      this.id = arguments[i]['id'];
-      this.modified = arguments[i]['modified'];
-      this.name = arguments[i]['name'];
-      this.resourceURI = arguments[i]['resourceURI'];
-      this.series = arguments[i]['series'];
-      this.stories = arguments[i]['stories'];
-      this.thumbnail = arguments[i]['thumbnail'];
-      this.urls = arguments[i].urls;
+class Herois{
+  constructor(array){
+    this.heroi = [];
+    for (let i = 0, len = array.length; i < len; i++) {
+      this.heroi[i] = array[i];
     }
   }
 }
