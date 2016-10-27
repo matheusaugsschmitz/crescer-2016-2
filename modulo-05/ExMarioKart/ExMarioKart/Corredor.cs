5ddf28a383018ec2efaa104a77e9b0e7fc29dd16
Matheus Schmitz <augs.math@gmail.com>
2016-10-27 17:28:43 -0200

    feat(Exercicio Mario Kart): INCOMPLETO

---
+++ modulo-05/ExMarioKart/ExMarioKart/Corredor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExMarioKart
+{
+    public class Corredor
+    {
+        public Corredor(string nome, string nivel = "Noob")
+        {
+            this.Nome = nome;
+            this.nivelHabilidade = nivel;
+        }
+        public string Nome { get; private set; }
+        private String nivelHabilidade;
+        public String NivelHabilidade
+        {
+            get
+            {
+                return this.nivelHabilidade;
+            }
+
+            set
+            {
+                bool ehUmNivelValido = "Noob".Equals(value) || "Mediano".Equals(value) || "Profissional".Equals(value);
+                if (ehUmNivelValido)
+                {
+                    this.nivelHabilidade = value;
+                }
+                else
+                {
+                    this.nivelHabilidade = "Noob";
+                }
+            }
+        }
+    }
+}
---
+++ modulo-05/ExMarioKart/ExMarioKartTests/CorredorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExMarioKart;
+
+namespace ExMarioKartTests
+{
+    [TestClass]
+    public class CorredorTest
+    {
+        [TestMethod]
+        public void CorredorComNivel1RetornaNivelNoob()
+        {
+            var corredor = new Corredor("Usain", "Noob");
+
+            Assert.AreEqual("Usain", corredor.Nome);
+            Assert.AreEqual("Noob", corredor.NivelHabilidade);
+        }
+    }
+}
