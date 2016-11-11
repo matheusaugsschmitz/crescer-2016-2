557e820117efdd8fe1ba5abebf9d4f694ad9db91
Matheus Schmitz <augs.math@gmail.com>
2016-11-11 15:54:10 -0200

    feat(MarvelFlix): "criar" site com login e listagem de personagens obtidos atraves de conexão com servidores da Marvel com paginação

---
+++ modulo-06/Marvelflix/Marvelflix.Repositorio/Repositorios/HeroisRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using Marvelflix.Dominio;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Marvelflix.Repositorio
+{
+    public class HeroisRepositorio : IHeroisRepositorio
+    {
+        public IEnumerable<Heroi> Todos(int pagina, int tamanhoPagina)
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                return contexto.Heroi
+                    .OrderBy(_ => _.Id)
+                    .Skip((pagina-1)*tamanhoPagina)
+                    .Take(tamanhoPagina)
+                    .ToList();
+            }
+        }
+
+        public Heroi PorId(int id)
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                return contexto.Heroi
+                    .Include(_ => _.Comics)
+                    .SingleOrDefault(_ => _.Id == id);
+            }
+        }
+
+        public void Adicionar(Heroi heroi)
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                contexto.Heroi.Add(heroi);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
