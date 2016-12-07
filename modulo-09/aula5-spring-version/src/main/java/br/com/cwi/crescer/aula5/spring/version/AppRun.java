2ec3b520cc034e06411169b87ab35cb732f1dac8
Matheus Schmitz <augs.math@gmail.com>
2016-12-07 03:46:09 -0200

    feat(Entidades): acrescentar entidades com mapeamento de persistencia.

---
+++ modulo-09/aula5-spring-version/src/main/java/br/com/cwi/crescer/aula5/spring/version/AppRun.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula5.spring.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author matha
+ */
+@SpringBootApplication
+public class AppRun {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppRun.class, args);
+    }
+
+}
